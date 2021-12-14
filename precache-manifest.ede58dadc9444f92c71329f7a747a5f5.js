ad6724dabba05ee1287bb45104170a6ce3156c00
castle-sky <familyboat@163.com>
2021-12-14 15:30:30 +0800

    Updates

---
+++ precache-manifest.ede58dadc9444f92c71329f7a747a5f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938da15a40cdfc83a5a91d913c0f1342",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cf7a1e23b04200cccad",
+    "url": "./static/css/main.5aded94c.chunk.css"
+  },
+  {
+    "revision": "83a1284d97e173525e61",
+    "url": "./static/js/2.3b5a3a0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3b5a3a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf7a1e23b04200cccad",
+    "url": "./static/js/main.db557415.chunk.js"
+  },
+  {
+    "revision": "48854900348d0d4e9cf2",
+    "url": "./static/js/runtime-main.ed723065.js"
+  }
+]);
